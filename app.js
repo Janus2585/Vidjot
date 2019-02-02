a86b3a14b10fcf8b40d35166262a31e6438b80cb
Janus2585 <bd.yhome@gmail.com>
2019-02-02 01:26:44 -0600

    Added success messages when adding, editing, and deleting ideas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -30,6 +32,26 @@ app.use(bodyParser.json());
 //Method override middleware
 app.use(methodOverride("_method"));
 
+//Express-session middleware
+app.use(
+  session({
+    secret: "swag",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Flash middleware
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 //Index Route
 app.get("/", (req, res) => {
   const title = "Vidjot";
@@ -43,79 +65,14 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-//Idea Index Page
-app.get("/ideas", (req, res) => {
-  Idea.find({})
-    .sort({ date: "desc" })
-    .then(ideas => {
-      res.render("ideas/index", {
-        ideas: ideas
-      });
-    });
+//User Login Route
+app.get("/users/login", (req, res) => {
+  res.send("login");
 });
 
-//Add Idea form
-app.get("/ideas/add", (req, res) => {
-  res.render("ideas/add");
-});
-
-//Edit Idea form
-app.get("/ideas/edit/:id", (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  }).then(idea => {
-    res.render("ideas/edit", {
-      idea: idea
-    });
-  });
-});
-
-//Process Form
-app.post("/ideas", (req, res) => {
-  let errors = [];
-  if (!req.body.title) {
-    errors.push({ text: "Please add a title" });
-  }
-  if (!req.body.details) {
-    errors.push({ text: "Please add details" });
-  }
-  if (errors.length > 0) {
-    res.render("ideas/add", {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details
-    };
-    new Idea(newUser).save().then(idea => {
-      res.redirect("/ideas");
-    });
-  }
-});
-
-//Edit Form Process
-app.put("/ideas/:id", (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  }).then(idea => {
-    //new values
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-
-    idea.save().then(() => {
-      res.redirect("/ideas");
-    });
-  });
-});
-
-//Delete Idea
-app.delete("/ideas/:id", (req, res) => {
-  Idea.remove({ _id: req.params.id }).then(() => {
-    res.redirect("/ideas");
-  });
+//User Register Route
+app.get("/users/register", (req, res) => {
+  res.send("register");
 });
 
 const port = 5000;
---
+++ routes/ideas.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
+
+//Idea Index Page
+router.get("/ideas", (req, res) => {
+  Idea.find({})
+    .sort({ date: "desc" })
+    .then(ideas => {
+      res.render("ideas/index", {
+        ideas: ideas
+      });
+    });
+});
+
+//Add Idea form
+router.get("/add", (req, res) => {
+  res.render("ideas/add");
+});
+
+//Edit Idea form
+router.get("/edit/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render("/edit", {
+      idea: idea
+    });
+  });
+});
+
+//Process Form
+router.post("/", (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please add details" });
+  }
+  if (errors.length > 0) {
+    res.render("/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser).save().then(idea => {
+      req.flash("success_msg", "Idea Added Successfully");
+      res.redirect("/ideas");
+    });
+  }
+});
+
+//Edit Form Process
+router.put("/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    //new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save().then(() => {
+      req.flash("success_msg", "Idea Edited Successfully");
+      res.redirect("/ideas");
+    });
+  });
+});
+
+//Delete Idea
+router.delete("/:id", (req, res) => {
+  Idea.remove({ _id: req.params.id }).then(() => {
+    req.flash("success_msg", "Idea Deleted Successfully");
+    res.redirect("/ideas");
+  });
+});
